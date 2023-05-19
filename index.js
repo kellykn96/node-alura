c186347eb2de83bdc45187c8e1bb419f061ef450
Kelly Knoblauch <kellyKnobla@gmail.com>
2023-05-19 14:07:13 -0300

    throw error

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+import fs from 'fs';
 import chalk from "chalk";
 
 console.log(chalk.bgWhiteBright('olá mundo'));
+
+// fs.readFile('package.json', 'utf-8', function (err,conteudo){
+//     console.log(conteudo)
+// })
+
+//usando dentro de uma função
+function trataErro(p){
+    throw new Error(chalk.redBright(p));
+};
+
+function leArquivo(nomeArquivo) {
+    fs.readFile(nomeArquivo, 'utf-8', function (err,conteudo){
+        if (err) {
+            trataErro(err)
+            console.log(err)
+        }else{
+            console.log(chalk.greenBright(conteudo))
+        }
+
+    })
+}
+
+leArquivo('package.jso');
+
+leArquivo('package.json')
